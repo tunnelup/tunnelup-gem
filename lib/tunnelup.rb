3bafdbb3c13fb8e862e13e40ddd8d13fcc132515
George Drummond <georgedrummond@gmail.com>
2017-05-10 18:12:53 -0400

    wip

--- lib/tunnelup.rb
+++ lib/tunnelup.rb
@@ -1,6 +1,8 @@
 require "tunnelup/version"
 
 module Tunnelup
+  autoload :Cli, 'tunnelup/cli'
+
   module Api
     autoload :User,   'tunnelup/api/user'
     autoload :Tunnel, 'tunnelup/api/tunnel'
--- lib/tunnelup/cli.rb
+++ lib/tunnelup/cli.rb
@@ -1,17 +1,19 @@
 require 'thor'
 
-class Cli < Thor
-  desc 'hello NAME', "Some app we have"
-  def hello(name)
-    puts "Hello there #{name}"
-  end
+module Tunnelup
+  class Cli < Thor
+    desc 'hello NAME', "Some app we have"
+    def hello(name)
+      puts "Hello there #{name}"
+    end
 
-  desc 'port PORT', 'some port'
-  def port(port)
-    puts "Your port is #{port}"
-  end
+    desc 'port PORT', 'some port'
+    option :port, desc: 'something for rescription', required: true, type: :numeric
+    option :host, desc: 'something for rescription', required: true, banner: 'something'
+    def port
+      puts "Your port is #{options[:port]}"
+    end
 
-  default_command :port
+    default_command :port
+  end
 end
-
-Cli.start(ARGV)
